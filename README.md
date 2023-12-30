decf41731510e86c0509aa26d9f010033e198d9a
mazeqbal <programmerblog.net@gmail.com>
2023-12-31 00:41:24 +0200

    learn react hooks

--- README.md
+++ README.md
@@ -48,3 +48,30 @@ import Component from "path"
 
 export const Component;
 import {Component} from "path"
+
+# React Hooks
+
+(Normal JS Utility function)
+
+- useState() - Superpowerful State Variable in React
+- useEffect()
+
+In useState, two parameters are given. Initial stae and a method to change the state. It will sync with UI. React is doing DOM manipulation and it is fast in re rendering the UI.
+
+`const [listOfRestaurants, setListOfRestaurants] = useState([])`
+
+Whenever a state variable updates, React re-renders the Component.
+
+React uses Reconciliation Algorithm in the backend. (React Fiber in React 16 - find diff and update the dom - incremental rendering)
+Why React is Fast. Because it is doing effecient dom manipulation because it has a virtual dom.
+It can find effeciently find diff between virtual dom.
+
+Whenever there is a UI. React creates a Virtual DOM of UI.
+
+Actual DOM has HTML tags like div and images etc.
+
+Virtual DOM is a representation of Actual DOM. Virtual DOM are react elements or Objects.
+
+### Diff Algorithm
+
+Diff Algorithm finds out the difference between the DOM and previous Virtual DOM.
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,28 @@
 import RestaurantCard from "./RestaurantCard"
-import restaurants from "../utils/mockData";
+import { useState } from "react";
+import restaurants from '../utils/mockData'
 
 const Body = () => {
+    //Local State Variable - uper Powrful variable - Hooks - useState
+    // A react hook is a normal JavaScript function
+    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+                <button
+                    className="filter-btn"
+                    onClick={() => {
+                        const filteredRestaurants = listOfRestaurants.filter(
+                            (res) => res.info.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredRestaurants);
+                    }}
+                >Top Rated Restaurants</button>
+            </div>
             <div className="res-container">
                 {
-                    restaurants.map(
+                    listOfRestaurants.map(
                         (restaurant) => (
                             <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                         )
