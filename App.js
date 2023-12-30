29c39dfad1c99c1fc43aa35d4fc397f45eca7827
mazeqbal <programmerblog.net@gmail.com>
2023-12-30 18:33:10 +0200

    Develop basic app structure

--- App.js
+++ App.js
@@ -11,39 +11,80 @@ import ReactDOM from 'react-dom/client';
  *  - Search
  *  - Restaurant Container
  *      - RestaurantCard
+ *          - Img
+ *          - Name of Res, Start Rating, Cuisine, delivery time
  * Footer
  *  - Copyright
  *  - Links
  *  - Address
  *  - Contact
  */
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
+const RestaurantCard = (props) => {
+    const { resData } = props;
+    const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } = resData?.info;
+    return (
+        <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+            <img className="res-logo"
+                alt="Tasty Food, s"
+                src={"https://media-assets.apidev.com/sw/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId} />
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{avgRating} stars</h4>
+            <h4>{costForTwo}</h4>
+            <h4>{sla.slaString}</h4>
+        </div>
+    )
+}
+const restaurants = [];
 
-/* const parent2 = React.createElement("div", { id: "parent" }, [
-     React.createElement("div", { id: "child" }, [
-         React.createElement("h1", {}, "I'm an h1 tag"),
-         React.createElement("h2", {}, "I'm an h2 tag"),
-     ]),
-     React.createElement("div", { id: "child2" }, [
-         React.createElement("h1", {}, "I'm an h1 tag"),
-         React.createElement("h2", {}, "I'm an h2 tag"),
-     ]),
- ]); */
-const Title = () => (
-    <h1 className="head" tabIndex="5">Hello React using JSX
-    </h1>
-);
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                {
+                    restaurants.map(
+                        (restaurant) => (
+                            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                        )
+                    )
+                }
+            </div>
+        </div>
+    )
+}
 
-const HeadingComponent = () => (
-    <div id="container">
-        <Title />
-        <h1>Hello React Functional component!</h1>
-    </div>
-);
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
 // Use createRoot to render the app
 const rootInstance = ReactDOM.createRoot(document.getElementById('root'));
-//rootInstance.render(parent);
-rootInstance.render(<HeadingComponent />);
+
+rootInstance.render(<AppLayout />);
 
 
 
