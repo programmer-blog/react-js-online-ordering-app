b45ba51f3d2421fb5c5fcf8caf342d7901490396
mazeqbal <programmerblog.net@gmail.com>
2024-01-02 14:50:37 +0200

    Use hooks useState, useEffect and shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,26 @@
 import RestaurantCard from "./RestaurantCard"
-import { useState } from "react";
-import restaurants from '../utils/mockData'
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
     //Local State Variable - uper Powrful variable - Hooks - useState
     // A react hook is a normal JavaScript function
-    const [listOfRestaurants, setListOfRestaurants] = useState(restaurants);
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-    return (
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.6244806999999999&page_type=DESKTOP_WEB_LISTING&is-seo-homepage-enabled=true");
+        const json = await data.json();
+
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    //conditional rendering
+    return listOfRestaurants.length == 0 
+        ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
                 <button
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+    const [btnNameReact, setBtnNameReact] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +15,14 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="login"
+                        onClick={() => {
+                            btnNameReact == "Login" ?
+                                setBtnNameReact("Logout") :
+                                setBtnNameReact("Login")
+                        }}>
+                        {btnNameReact}
+                    </button>
                 </ul>
             </div>
         </div>
